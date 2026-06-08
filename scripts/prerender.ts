4d8101fd43c6521bab110499f68e4312715f156f
fknmiz <150495873+fknmiz@users.noreply.github.com>
2026-06-08 16:33:36 -0400

    Enrich public evidence surface

--- scripts/prerender.ts
+++ scripts/prerender.ts
@@ -6,3 +6,5 @@ const input = JSON.parse(readFileSync("fixtures/hft-latency-sample.json", "utf8"
 mkdirSync("site", { recursive: true });
 writeFileSync("site/index.html", renderPage(input));
 writeFileSync("site/robots.txt", "User-agent: *\nAllow: /\n");
+
+writeFileSync("site/sitemap.xml", "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<urlset xmlns=\"http://www.sitemaps.org/schemas/sitemap/0.9\">\n  <url><loc>https://mizcausevic-dev.github.io/hft-latency-risk-ledger/</loc></url>\n</urlset>\n");
